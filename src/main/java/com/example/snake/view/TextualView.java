c4092932f28279034697ec481c023238f421829b
Emanuele <egalli64@gmail.com>
2022-04-06 21:25:06 +0200

    intellij

--- src/main/java/com/example/snake/view/TextualView.java
+++ src/main/java/com/example/snake/view/TextualView.java
@@ -6,9 +6,10 @@ import org.tinylog.Logger;
 
 public class TextualView {
     public boolean go() {
-        System.out.println("Enter when ready");
+        System.out.print("Enter when ready > ");
         try(Scanner scanner = new Scanner(System.in)) {
-            Logger.trace(scanner.nextLine());
+            String input = scanner.nextLine();
+            Logger.trace(input);
         }
 
         return true;
